b525f7d00f6138112ba1b7d7303b77dfb8de1e55
leixing <leixing1012@qq.com>
2020-10-19 00:57:50 +0800

    开放包

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,10 @@
 module Csv {
     requires commons.csv;
     requires java.sql;
+
+    opens com.xingray.csv.annotations;
+    exports com.xingray.csv.annotations;
+
+    opens com.xingray.csv;
+    exports com.xingray.csv;
 }
